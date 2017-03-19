559ea9a7ebcb7fb5b9bb55b5b4a77992d8eaad97
Brett Wagner <daishi4u@yahoo.com>
2017-03-19 15:57:39 -0500

    remove intelliplug to fix reboots

--- arch/arm64/hotplug/THUNDERPLUG/thunderplug.c
+++ arch/arm64/hotplug/THUNDERPLUG/thunderplug.c
@@ -9,48 +9,82 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * A simple hotplugging driver optimized for Octa Core CPUs
+ * A simple hotplugging driver.
+ * Compatible from dual core CPUs to Octa Core CPUs
  */
 
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/hrtimer.h>
 #include <linux/init.h>
 #include <linux/device.h>
 #include <linux/input.h>
 #include <linux/slab.h>
 #include <linux/cpu.h>
-#include <linux/lcd_notify.h>
 #include <linux/cpufreq.h>
+#include <linux/powersuspend.h>
+#include "thunderplug.h"
 
-static int suspend_cpu_num = 1, resume_cpu_num = 7;
-static int endurance_level = 0;
-static int device_cpus = 8;
-static int core_limit = 8;
+#define DEBUG                        0
+
+#define THUNDERPLUG                  "thunderplug"
+
+#ifdef CONFIG_SCHED_HMP
+#define DRIVER_VERSION                5
+#else
+#define DRIVER_VERSION                3
+#define DRIVER_SUBVER                 0
+#endif
+
+#define DEFAULT_CPU_LOAD_THRESHOLD   (65)
+#define MIN_CPU_LOAD_THRESHOLD       (10)
+
+#ifdef CONFIG_USES_MALI_MP2_GPU
+#define GPU_HOTPLUG_ENABLED             (0)
+#define DEFAULT_MIN_GPU_LOAD_THRESHOLD  (65)
+#endif
+
+#define HOTPLUG_ENABLED              (0)
+#define DEFAULT_HOTPLUG_STYLE         HOTPLUG_SCHED
+#define DEFAULT_SCHED_MODE            BALANCED
+
+#define DEF_SAMPLING_MS	             (500)
+#define MIN_SAMLING_MS               (50)
+#define MIN_CPU_UP_TIME              (750)
+#define TOUCH_BOOST_ENABLED          (0)
 
 static bool isSuspended = false;
 
-struct notifier_block lcd_worker;
-
-#define DEBUG 0
-
-#define THUNDERPLUG "thunderplug"
-
-#define DRIVER_VERSION  2
-#define DRIVER_SUBVER 5
-
-#define CPU_LOAD_THRESHOLD        (65)
-
-#define DEF_SAMPLING_MS			(500)
-#define MIN_CPU_UP_TIME			(750)
+static int suspend_cpu_num = 2, resume_cpu_num = (NR_CPUS -1);
+static int endurance_level = 0;
+static int core_limit = NR_CPUS;
 
 static int now[8], last_time[8];
 
 static int sampling_time = DEF_SAMPLING_MS;
-static int load_threshold = CPU_LOAD_THRESHOLD;
+static int load_threshold = DEFAULT_CPU_LOAD_THRESHOLD;
+static int stop_boost = 0;
 
-static int tplug_hp_enabled = 0;
+#ifdef CONFIG_USES_MALI_MP2_GPU
+extern void gpu_enable(int num);
+extern u32 _mali_ukk_utilization_gp_pp(void);
+extern u32 mali_pp_scheduler_get_num_cores_enabled(void);
+#endif
 
-static int touch_boost_enabled = 0;
+struct cpufreq_policy old_policy[NR_CPUS];
+
+#ifdef CONFIG_SCHED_HMP
+static int tplug_hp_style = DEFAULT_HOTPLUG_STYLE;
+static int tplug_sched_mode = DEFAULT_SCHED_MODE;
+#else
+static int tplug_hp_enabled = HOTPLUG_ENABLED;
+#endif
+static int touch_boost_enabled = TOUCH_BOOST_ENABLED;
+
+#ifdef CONFIG_USES_MALI_MP2_GPU
+static int gpu_hotplug_enabled = GPU_HOTPLUG_ENABLED;
+static int gpu_min_load_threshold = DEFAULT_MIN_GPU_LOAD_THRESHOLD;
+#endif
 
 static struct workqueue_struct *tplug_wq;
 static struct delayed_work tplug_work;
@@ -61,7 +95,7 @@ static struct delayed_work tplug_boost;
 static struct workqueue_struct *tplug_resume_wq;
 static struct delayed_work tplug_resume_work;
 
-static unsigned int last_load[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+static unsigned int last_load[8] = { 0 };
 
 struct cpu_load_data {
 	u64 prev_cpu_idle;
@@ -75,26 +109,31 @@ struct cpu_load_data {
 
 static DEFINE_PER_CPU(struct cpu_load_data, cpuload);
 
+
+/* Two Endurance Levels for Octa Cores,
+ * Two for Quad Cores and
+ * One for Dual
+ */
 static inline void offline_cpus(void)
 {
 	unsigned int cpu;
 	switch(endurance_level) {
 		case 1:
-			if(suspend_cpu_num > 4)
-				suspend_cpu_num = 4;
+			if(suspend_cpu_num > NR_CPUS / 2 )
+				suspend_cpu_num = NR_CPUS / 2;
 		break;
 		case 2:
-			if(suspend_cpu_num > 2)
-				suspend_cpu_num = 2;
+			if( NR_CPUS >=4 && suspend_cpu_num > NR_CPUS / 4)
+				suspend_cpu_num = NR_CPUS / 4;
 		break;
 		default:
 		break;
 	}
-	for(cpu = 7; cpu > (suspend_cpu_num - 1); cpu--) {
+	for(cpu = NR_CPUS - 1; cpu > (suspend_cpu_num - 1); cpu--) {
 		if (cpu_online(cpu))
 			cpu_down(cpu);
 	}
-	pr_info("%s: %d cpus were offlined\n", THUNDERPLUG, (device_cpus - suspend_cpu_num));
+	pr_info("%s: %d cpus were offlined\n", THUNDERPLUG, (NR_CPUS - suspend_cpu_num));
 }
 
 static inline void cpus_online_all(void)
@@ -102,20 +141,22 @@ static inline void cpus_online_all(void)
 	unsigned int cpu;
 	switch(endurance_level) {
 	case 1:
-		if(resume_cpu_num > 3 || resume_cpu_num == 1)
-			resume_cpu_num = 3;
+		if(resume_cpu_num > (NR_CPUS / 2) - 1 || resume_cpu_num == 1)
+			resume_cpu_num = ((NR_CPUS / 2) - 1);
 	break;
 	case 2:
-		if(resume_cpu_num > 1)
-			resume_cpu_num = 1;
+		if( NR_CPUS >= 4 && resume_cpu_num > ((NR_CPUS / 4) - 1))
+			resume_cpu_num = ((NR_CPUS / 4) - 1);
 	break;
 	case 0:
-		if(resume_cpu_num < 7)
-			resume_cpu_num = 7;
+			resume_cpu_num = (NR_CPUS - 1);
 	break;
 	default:
 	break;
 	}
+
+	if(DEBUG)
+		   pr_info("%s: resume_cpu_num = %d\n",THUNDERPLUG, resume_cpu_num);
 
 	for (cpu = 1; cpu <= resume_cpu_num; cpu++) {
 		if (cpu_is_offline(cpu))
@@ -127,19 +168,52 @@ static inline void cpus_online_all(void)
 
 static void __ref tplug_boost_work_fn(struct work_struct *work)
 {
-	int cpu;
-	for(cpu = 1; cpu < 4; cpu++) {
+	struct cpufreq_policy policy;
+	int cpu, ret;
+	for(cpu = 1; cpu < NR_CPUS; cpu++) {
+#ifdef CONFIG_SCHED_HMP
+    if(tplug_hp_style == 1)
+#else
+	if(tplug_hp_enabled == 1)
+#endif
 		if(cpu_is_offline(cpu))
 			cpu_up(cpu);
+		ret = cpufreq_get_policy(&policy, cpu);
+		if (ret)
+			continue;
+		old_policy[cpu] = policy;
+		policy.min = policy.max;
+		cpufreq_update_policy(cpu);
 	}
+	if(stop_boost == 0)
+	queue_delayed_work_on(0, tplug_boost_wq, &tplug_boost,
+			msecs_to_jiffies(10));
 }
 
 static void tplug_input_event(struct input_handle *handle, unsigned int type,
 		unsigned int code, int value)
 {
-
+	if (type == EV_KEY && code == BTN_TOUCH) {
+		if(DEBUG)
+			pr_info("%s : type = %d, code = %d, value = %d\n", THUNDERPLUG, type, code, value);
+		if(value == 0) {
+			stop_boost = 1;
+			if(DEBUG)
+				pr_info("%s: stopping boost\n", THUNDERPLUG);
+		}
+		else {
+			stop_boost = 0;
+			if(DEBUG)
+				pr_info("%s: starting boost\n", THUNDERPLUG);
+		}
+	}
+#ifdef CONFIG_SCHED_HMP
+    if ((type == EV_KEY) && (code == BTN_TOUCH) && (value == 1)
+		&& touch_boost_enabled == 1)
+#else
 	if ((type == EV_KEY) && (code == BTN_TOUCH) && (value == 1)
-		&& touch_boost_enabled == 1 && tplug_hp_enabled == 1)
+		&& touch_boost_enabled == 1)
+#endif
 	{
 		if(DEBUG)
 			pr_info("%s : touch boost\n", THUNDERPLUG);
@@ -207,7 +281,7 @@ static ssize_t thunderplug_suspend_cpus_store(struct kobject *kobj, struct kobj_
 {
 	int val;
 	sscanf(buf, "%d", &val);
-	if(val < 1 || val > 8)
+	if(val < 1 || val > NR_CPUS)
 		pr_info("%s: suspend cpus off-limits\n", THUNDERPLUG);
 	else
 		suspend_cpu_num = val;
@@ -224,11 +298,17 @@ static ssize_t __ref thunderplug_endurance_store(struct kobject *kobj, struct ko
 {
 	int val;
 	sscanf(buf, "%d", &val);
+#ifdef CONFIG_SCHED_HMP
+	if(tplug_hp_style==1) {
+#else
+	if(tplug_hp_enabled) {
+#endif
 	switch(val) {
 	case 0:
 	case 1:
 	case 2:
-		if(endurance_level!=val) {
+		if(endurance_level!=val &&
+		   !(endurance_level > 1 && NR_CPUS < 4)) {
 		endurance_level = val;
 		offline_cpus();
 		cpus_online_all();
@@ -238,6 +318,9 @@ static ssize_t __ref thunderplug_endurance_store(struct kobject *kobj, struct ko
 		pr_info("%s: invalid endurance level\n", THUNDERPLUG);
 	break;
 	}
+	}
+	else
+	   pr_info("%s: per-core hotplug style is disabled, ignoring endurance mode values\n", THUNDERPLUG);
 
 	return count;
 }
@@ -251,36 +334,8 @@ static ssize_t __ref thunderplug_sampling_store(struct kobject *kobj, struct kob
 {
 	int val;
 	sscanf(buf, "%d", &val);
-	if(val > 50)
+	if(val > MIN_SAMLING_MS)
 		sampling_time = val;
-
-	return count;
-}
-
-static ssize_t thunderplug_hp_enabled_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
-{
-    return sprintf(buf, "%d", tplug_hp_enabled);
-}
-
-static ssize_t __ref thunderplug_hp_enabled_store(struct kobject *kobj, struct kobj_attribute *attr, const char *buf, size_t count)
-{
-	int val;
-	int last_val = tplug_hp_enabled;	
-	sscanf(buf, "%d", &val);
-	switch(val)
-	{
-		case 0:
-		case 1:
-			tplug_hp_enabled = val;
-		break;
-		default:
-			pr_info("%s : invalid choice\n", THUNDERPLUG);
-		break;
-	}
-
-	if(tplug_hp_enabled == 1 && tplug_hp_enabled != last_val)
-		queue_delayed_work_on(0, tplug_wq, &tplug_work,
-							msecs_to_jiffies(sampling_time));
 
 	return count;
 }
@@ -323,6 +378,40 @@ static ssize_t __ref thunderplug_load_store(struct kobject *kobj, struct kobj_at
 	return count;
 }
 
+#ifdef CONFIG_USES_MALI_MP2_GPU
+static ssize_t thunderplug_gpu_load_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
+{
+    return sprintf(buf, "%d", gpu_min_load_threshold);
+}
+
+static ssize_t __ref thunderplug_gpu_load_store(struct kobject *kobj, struct kobj_attribute *attr, const char *buf, size_t count)
+{
+	int val;
+	sscanf(buf, "%d", &val);
+	if(val > 10)
+		gpu_min_load_threshold = val;
+
+	return count;
+}
+
+static unsigned int get_gpu_load(void) {
+	int util, load;
+	util = (int) _mali_ukk_utilization_gp_pp();
+	load = (util * 100 ) / 256;
+	return load;
+}
+
+static int get_gpu_cores_enabled(void) {
+	int cores;
+	cores = (int) mali_pp_scheduler_get_num_cores_enabled();
+	return cores;
+}
+
+static void enable_gpu_cores(int num) {
+	gpu_enable(num);
+}
+#endif
+
 static unsigned int get_curr_load(unsigned int cpu)
 {
 	int ret;
@@ -364,16 +453,16 @@ static void __cpuinit tplug_work_fn(struct work_struct *work)
 	switch(endurance_level)
 	{
 	case 0:
-		core_limit = 8;
+		core_limit = NR_CPUS;
 	break;
 	case 1:
-		core_limit = 4;
+		core_limit = NR_CPUS / 2;
 	break;
 	case 2:
-		core_limit = 2;
+		core_limit = NR_CPUS / 4;
 	break;
 	default:
-		core_limit = 8;
+		core_limit = NR_CPUS;
 	break;
 	}
 
@@ -410,8 +499,32 @@ static void __cpuinit tplug_work_fn(struct work_struct *work)
 			}
 		}
 	}
+#ifdef CONFIG_USES_MALI_MP2_GPU
+	if(gpu_hotplug_enabled) {
+		if(DEBUG)
+			pr_info("%s: current gpu load %d\n", THUNDERPLUG, get_gpu_load());
+		if(get_gpu_load() > gpu_min_load_threshold) {
+			if(get_gpu_cores_enabled() < 2) {
+				enable_gpu_cores(2);
+				if(DEBUG)
+					pr_info("%s: gpu1 onlined\n", THUNDERPLUG);
+			}
+		}
+		else {
+			if(get_gpu_cores_enabled() > 1) {
+				enable_gpu_cores(1);
+				if(DEBUG)
+					pr_info("%s: gpu1 offlined\n", THUNDERPLUG);
+			}
+		}
+	}
+#endif
 
+#ifdef CONFIG_SCHED_HMP
+    if(tplug_hp_style == 1 && !isSuspended)
+#else
 	if(tplug_hp_enabled != 0 && !isSuspended)
+#endif
 		queue_delayed_work_on(0, tplug_wq, &tplug_work,
 			msecs_to_jiffies(sampling_time));
 	else {
@@ -423,34 +536,177 @@ static void __cpuinit tplug_work_fn(struct work_struct *work)
 
 }
 
-static int lcd_notifier_callback(struct notifier_block *nb,
-                                 unsigned long event, void *data)
-{
-       switch (event) {
-       case LCD_EVENT_ON_START:
-			isSuspended = false;
-			if(tplug_hp_enabled)
-				queue_delayed_work_on(0, tplug_wq, &tplug_work,
-								msecs_to_jiffies(sampling_time));
-			else
-				queue_delayed_work_on(0, tplug_resume_wq, &tplug_resume_work,
-		                      msecs_to_jiffies(10));
-			pr_info("thunderplug : resume called\n");
-               break;
-       case LCD_EVENT_ON_END:
-               break;
-       case LCD_EVENT_OFF_START:
-               break;
-       case LCD_EVENT_OFF_END:
-			isSuspended = true;
-			pr_info("thunderplug : suspend called\n");
-               break;
-       default:
-               break;
-       }
-
-       return 0;
+static void tplug_es_suspend_work(struct power_suspend *p) {
+	isSuspended = true;
+	pr_info("thunderplug : suspend called\n");
 }
+
+static void tplug_es_resume_work(struct power_suspend *p) {
+	isSuspended = false;
+#ifdef CONFIG_SCHED_HMP
+	if(tplug_hp_style==1)
+#else
+	if(tplug_hp_enabled)
+#endif
+	queue_delayed_work_on(0, tplug_wq, &tplug_work,
+					msecs_to_jiffies(sampling_time));
+	else
+		queue_delayed_work_on(0, tplug_resume_wq, &tplug_resume_work,
+		            msecs_to_jiffies(10));
+	pr_info("thunderplug : resume called\n");
+}
+
+static struct power_suspend tplug_power_suspend_handler = 
+	{
+		.suspend = tplug_es_suspend_work,
+		.resume = tplug_es_resume_work,
+	};
+
+/* Thunderplug load balancer */
+#ifdef CONFIG_SCHED_HMP
+
+static void set_sched_profile(int mode) {
+    switch(mode) {
+	   case 1:
+	       /* Balanced */
+	       sched_set_boost(DISABLED);
+	   break;
+	   case 2:
+	       /* Turbo */
+	       sched_set_boost(ENABLED);
+	   break;
+	   default:
+	       pr_info("%s: Invalid mode\n", THUNDERPLUG);
+	   break;
+	}
+}
+
+static ssize_t thunderplug_sched_mode_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
+{
+    return sprintf(buf, "%d", tplug_sched_mode);
+}
+
+static ssize_t __ref thunderplug_sched_mode_store(struct kobject *kobj, struct kobj_attribute *attr, const char *buf, size_t count)
+{
+	int val;
+	sscanf(buf, "%d", &val);
+	set_sched_profile(val);
+	tplug_sched_mode = val;
+	return count;
+}
+
+static ssize_t thunderplug_hp_style_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
+{
+    return sprintf(buf, "%d", tplug_hp_style);
+}
+
+static ssize_t __ref thunderplug_hp_style_store(struct kobject *kobj, struct kobj_attribute *attr, const char *buf, size_t count)
+{
+	int val, last_val;
+	sscanf(buf, "%d", &val);
+	last_val = tplug_hp_style;
+	switch(val)
+	{
+		case HOTPLUG_PERCORE:
+		case HOTPLUG_SCHED:
+			   tplug_hp_style = val;
+		break;
+		default:
+			pr_info("%s : invalid choice\n", THUNDERPLUG);
+		break;
+	}
+
+	if(tplug_hp_style == HOTPLUG_PERCORE && tplug_hp_style != last_val) {
+	    pr_info("%s: Switching to Per-core hotplug model\n", THUNDERPLUG);
+	    sched_set_boost(DISABLED);
+		queue_delayed_work_on(0, tplug_wq, &tplug_work,
+							msecs_to_jiffies(sampling_time));
+	}
+	else if(tplug_hp_style==2) {
+	    pr_info("%s: Switching to sched based hotplug model\n", THUNDERPLUG);
+	    set_sched_profile(tplug_sched_mode);
+	}
+
+	return count;
+}
+
+static struct kobj_attribute thunderplug_hp_style_attribute =
+       __ATTR(hotplug_style,
+               0666,
+               thunderplug_hp_style_show, thunderplug_hp_style_store);
+
+static struct kobj_attribute thunderplug_mode_attribute =
+       __ATTR(sched_mode,
+               0666,
+               thunderplug_sched_mode_show, thunderplug_sched_mode_store);
+
+#else
+static ssize_t thunderplug_hp_enabled_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
+{
+    return sprintf(buf, "%d", tplug_hp_enabled);
+}
+
+static ssize_t __ref thunderplug_hp_enabled_store(struct kobject *kobj, struct kobj_attribute *attr, const char *buf, size_t count)
+{
+	int val;
+	int last_val = tplug_hp_enabled;
+	sscanf(buf, "%d", &val);
+	switch(val)
+	{
+		case 0:
+		case 1:
+			tplug_hp_enabled = val;
+		break;
+		default:
+			pr_info("%s : invalid choice\n", THUNDERPLUG);
+		break;
+	}
+
+	if(tplug_hp_enabled == 1 && tplug_hp_enabled != last_val)
+		queue_delayed_work_on(0, tplug_wq, &tplug_work,
+							msecs_to_jiffies(sampling_time));
+
+	return count;
+}
+
+#ifdef CONFIG_USES_MALI_MP2_GPU
+static ssize_t thunderplug_gpu_hp_enabled_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
+{
+    return sprintf(buf, "%d", gpu_hotplug_enabled);
+}
+
+static ssize_t __ref thunderplug_gpu_hp_enabled_store(struct kobject *kobj, struct kobj_attribute *attr, const char *buf, size_t count)
+{
+	int val;
+	sscanf(buf, "%d", &val);
+	switch(val)
+	{
+		case 0:
+		case 1:
+			gpu_hotplug_enabled = val;
+		break;
+		default:
+			pr_info("%s : invalid choice\n", THUNDERPLUG);
+		break;
+	}
+
+	return count;
+}
+#endif
+
+static struct kobj_attribute thunderplug_hp_enabled_attribute =
+       __ATTR(hotplug_enabled,
+               0666,
+               thunderplug_hp_enabled_show, thunderplug_hp_enabled_store);
+
+#ifdef CONFIG_USES_MALI_MP2_GPU
+static struct kobj_attribute thunderplug_gpu_hp_enabled_attribute =
+       __ATTR(gpu_hotplug_enabled,
+               0666,
+               thunderplug_gpu_hp_enabled_show, thunderplug_gpu_hp_enabled_store);
+#endif  // MALI_MP2_GPU
+
+#endif  //SCHED_HMP
 
 static ssize_t thunderplug_ver_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
 {
@@ -482,10 +738,12 @@ static struct kobj_attribute thunderplug_load_attribute =
                0666,
                thunderplug_load_show, thunderplug_load_store);
 
-static struct kobj_attribute thunderplug_hp_enabled_attribute =
-       __ATTR(hotplug_enabled,
+#ifdef CONFIG_USES_MALI_MP2_GPU
+static struct kobj_attribute thunderplug_gpu_load_attribute =
+       __ATTR(gpu_load_threshold,
                0666,
-               thunderplug_hp_enabled_show, thunderplug_hp_enabled_store);
+               thunderplug_gpu_load_show, thunderplug_gpu_load_store);
+#endif
 
 static struct kobj_attribute thunderplug_tb_enabled_attribute =
        __ATTR(touch_boost,
@@ -499,8 +757,17 @@ static struct attribute *thunderplug_attrs[] =
         &thunderplug_endurance_attribute.attr,
         &thunderplug_sampling_attribute.attr,
         &thunderplug_load_attribute.attr,
+#ifdef CONFIG_SCHED_HMP
+        &thunderplug_mode_attribute.attr,
+        &thunderplug_hp_style_attribute.attr,
+#else
         &thunderplug_hp_enabled_attribute.attr,
+#endif
         &thunderplug_tb_enabled_attribute.attr,
+#ifdef CONFIG_USES_MALI_MP2_GPU
+        &thunderplug_gpu_load_attribute.attr,
+        &thunderplug_gpu_hp_enabled_attribute.attr,
+#endif
         NULL,
     };
 
@@ -516,6 +783,7 @@ static int __init thunderplug_init(void)
         int ret = 0;
         int sysfs_result;
         printk(KERN_DEBUG "[%s]\n",__func__);
+		register_power_suspend(&tplug_power_suspend_handler);
 
         thunderplug_kobj = kobject_create_and_add("thunderplug", kernel_kobj);
 
@@ -531,10 +799,6 @@ static int __init thunderplug_init(void)
                 pr_info("%s sysfs create failed!\n", __FUNCTION__);
                 kobject_put(thunderplug_kobj);
         }
-
-		lcd_worker.notifier_call = lcd_notifier_callback;
-
-        
 
 		pr_info("%s : registering input boost", THUNDERPLUG);
 		ret = input_register_handler(&tplug_input_handler);
@@ -565,5 +829,5 @@ static int __init thunderplug_init(void)
 
 MODULE_LICENSE("GPL and additional rights");
 MODULE_AUTHOR("Varun Chitre <varun.chitre15@gmail.com>");
-MODULE_DESCRIPTION("Hotplug driver for OctaCore CPU");
+MODULE_DESCRIPTION("Hotplug driver for ARM SoCs");
 late_initcall(thunderplug_init);
